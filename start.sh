2d3c94c13992526ca83205b133b942e9467626b1
Kevo <me@kevo.link>
2022-05-09 08:48:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@ cd "$(dirname "$0")"
 
 export FLASK_APP="python_web_project_template.web:WEB"
 
-# XXX: Ctrl+C will only terminate Uvicorn, Flask will keep running in the background (Windows, Git Bash). If you know a way around this, please make a PR, thanks!
-flask run --host 0.0.0.0 --port 5000 & uvicorn python_web_project_template.web.api:API --host 0.0.0.0 --port 5001
+(flask run --host 0.0.0.0 --port 5000 & uvicorn python_web_project_template.web.api:API --host 0.0.0.0 --port 5001)
